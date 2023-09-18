e4b29ef96e2ca0f0af8ca4335924fe41c556fc86
thang-39 <91838559+thang-39@users.noreply.github.com>
2023-09-18 14:02:43 +0700

    2 ways binding with value

--- section5-state-and-events/src/components/NewExpense/ExpenseForm.jsx
+++ section5-state-and-events/src/components/NewExpense/ExpenseForm.jsx
@@ -18,22 +18,49 @@ const ExpenseForm = () => {
   const dateChangeHandler = (event) => {
     setEnteredDate(event.target.value);
   };
+
+//   const inputChangeHandler = (identifier, value) => {
+//     if (identifier === 'title') setEnteredTitle(value);
+//     else if (identifier === 'date') setEnteredDate(value);
+//     else setEnteredAmount(value);
+//   }
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+    const expenseData = {
+        title: enteredTitle,
+        amount: enteredAmount,
+        date: new Date(enteredDate),
+    };
+
+    console.log(expenseData);
+    setEnteredTitle('');
+    setEnteredAmount('');
+    setEnteredDate('')
+  };
  
   return (
-    <form>
+    <form onSubmit={submitHandler}>
       <div className='new-expense__controls'>
         <div className='new-expense__control'>
           <label>Title</label>
-          <input type='text' onChange={titleChangeHandler}
+          <input 
+            type='text'
+            value={enteredTitle}
+            onChange={titleChangeHandler}
           />
         </div>
         <div className='new-expense__control'>
           <label>Amount</label>
-          <input type='number' min='0.01' step='0.01' onChange={amountChangeHandler}/>
+          <input type='number' min='0.01' step='0.01'
+            value={enteredAmount}
+            onChange={amountChangeHandler}/>
         </div>
         <div className='new-expense__control'>
           <label>Date</label>
-          <input type='date' min="2019-01-01" max='2023-12-31' onChange={dateChangeHandler}/>
+          <input type='date' min="2019-01-01" max='2023-12-31'
+            value={enteredDate}
+            onChange={dateChangeHandler}/>
         </div>
         <div className='new-expense__actions'>
           <button type='submit'>Add Expense</button>
