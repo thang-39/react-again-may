40f5260519aa1fa8f8a49ead9d02970a4ac29502
thang-39 <91838559+thang-39@users.noreply.github.com>
2023-09-21 15:22:08 +0700

    prevent props chains with Context - use Provider and Consumer

--- section11-Effect-Reducer/src/App.js
+++ section11-Effect-Reducer/src/App.js
@@ -3,6 +3,7 @@ import React, { useState, useEffect } from 'react';
 import Login from './components/Login/Login';
 import Home from './components/Home/Home';
 import MainHeader from './components/MainHeader/MainHeader';
+import AuthContext from './store/auth-context';
 
 function App() {
 
@@ -19,7 +20,7 @@ function App() {
   const loginHandler = (email, password) => {
     // We should of course check email and password
     // But it's just a dummy/ demo anyways
-    localStorage.setItem('isLoggedIn','1');
+    localStorage.setItem('isLoggedIn', '1');
     setIsLoggedIn(true);
   };
 
@@ -29,13 +30,17 @@ function App() {
   };
 
   return (
-    <React.Fragment>
-      <MainHeader isAuthenticated={isLoggedIn} onLogout={logoutHandler} />
+    <AuthContext.Provider value={{
+      isLoggedIn: isLoggedIn,
+    }}>
+      <MainHeader 
+        // isAuthenticated={isLoggedIn} 
+        onLogout={logoutHandler} />
       <main>
         {!isLoggedIn && <Login onLogin={loginHandler} />}
         {isLoggedIn && <Home onLogout={logoutHandler} />}
       </main>
-    </React.Fragment>
+    </AuthContext.Provider>
   );
 }
 
