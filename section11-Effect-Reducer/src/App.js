b1c4e657e97ed55f64eaacdf566813cf496f2ee7
thang-39 <91838559+thang-39@users.noreply.github.com>
2023-09-21 16:59:28 +0700

    Building & Using a Custom Context Provider Component

--- section11-Effect-Reducer/src/App.js
+++ section11-Effect-Reducer/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useContext } from 'react';
 
 import Login from './components/Login/Login';
 import Home from './components/Home/Home';
@@ -7,41 +7,46 @@ import AuthContext from './store/auth-context';
 
 function App() {
 
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  // const [isLoggedIn, setIsLoggedIn] = useState(false);
 
-  useEffect(() => {
-    const storedUserLoggedInInfomation = localStorage.getItem('isLoggedIn');
+  // useEffect(() => {
+  //   const storedUserLoggedInInfomation = localStorage.getItem('isLoggedIn');
 
-    if (storedUserLoggedInInfomation === '1') {
-      setIsLoggedIn(true);
-    }
-  }, []);
+  //   if (storedUserLoggedInInfomation === '1') {
+  //     setIsLoggedIn(true);
+  //   }
+  // }, []);
 
-  const loginHandler = (email, password) => {
-    // We should of course check email and password
-    // But it's just a dummy/ demo anyways
-    localStorage.setItem('isLoggedIn', '1');
-    setIsLoggedIn(true);
-  };
+  // const loginHandler = (email, password) => {
+  //   // We should of course check email and password
+  //   // But it's just a dummy/ demo anyways
+  //   localStorage.setItem('isLoggedIn', '1');
+  //   setIsLoggedIn(true);
+  // };
 
-  const logoutHandler = () => {
-    localStorage.removeItem('isLoggedIn')
-    setIsLoggedIn(false);
-  };
+  // const logoutHandler = () => {
+  //   localStorage.removeItem('isLoggedIn')
+  //   setIsLoggedIn(false);
+  // };
+
+  const ctx = useContext(AuthContext);
 
   return (
-    <AuthContext.Provider value={{
-      isLoggedIn: isLoggedIn,
-      onLogout: logoutHandler
-    }}>
-      <MainHeader 
+    // <AuthContext.Provider value={{
+    //   isLoggedIn: isLoggedIn,
+    //   onLogout: logoutHandler
+    // }}>
+    <>
+      <MainHeader
         // isAuthenticated={isLoggedIn} 
-        onLogout={logoutHandler} />
+        // onLogout={logoutHandler}    
+      />
       <main>
-        {!isLoggedIn && <Login onLogin={loginHandler} />}
-        {isLoggedIn && <Home onLogout={logoutHandler} />}
+        {!ctx.isLoggedIn && <Login />}
+        {ctx.isLoggedIn && <Home />}
       </main>
-    </AuthContext.Provider>
+    </>
+    // </AuthContext.Provider>
   );
 }
 
--- section11-Effect-Reducer/src/components/Login/Login.js
+++ section11-Effect-Reducer/src/components/Login/Login.js
@@ -1,8 +1,9 @@
-import React, { useEffect, useState, useReducer } from 'react';
+import React, { useEffect, useState, useReducer, useContext } from 'react';
 
 import Card from '../UI/Card/Card';
 import classes from './Login.module.css';
 import Button from '../UI/Button/Button';
+import AuthContext from '../../store/auth-context';
 
 const emailReducer = (state, action) => {
   if (action.type === 'USER_INPUT') {
@@ -35,6 +36,8 @@ const Login = (props) => {
 
   const [emailState, dispatchEmail] = useReducer(emailReducer, { value: '', isValid: null });
   const [passwordState, dispatchPassword] = useReducer(passwordReducer, { value: '', isValid: null });
+
+  const authCtx = useContext(AuthContext);
 
   useEffect(() => {
     console.log('Effect RUNNING');
@@ -90,7 +93,7 @@ const Login = (props) => {
 
   const submitHandler = (event) => {
     event.preventDefault();
-    props.onLogin(emailState.value, passwordState.value);
+    authCtx.onLogin(emailState.value, passwordState.value);
   };
 
   return (
